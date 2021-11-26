f1c34f368ee4ba47ec3a4d1ed32e41610a013bfe
JeremyKoolios <jeremy.koch@xtra.co.nz>
2021-11-27 12:25:31 +1300

    setup base command handler

--- Program.cs
+++ Program.cs
@@ -15,6 +15,8 @@ namespace DiscordMusicBot
         {
             //initalize application
             _client = new DiscordSocketClient();
+
+            // MessageReceived event triggers when someone writes a message
             _client.MessageReceived += CommandHandler;
             _client.Log += Log;
 
@@ -33,8 +35,23 @@ namespace DiscordMusicBot
             return Task.CompletedTask;
         }
 
+        // Receives and processes every incoming discord message
         private Task CommandHandler(SocketMessage msg)
         {
+            string cmd = "";
+            int cmdLength = -1;
+
+            if (!msg.Content.StartsWith('!') ||
+                msg.Author.IsBot)
+                return Task.CompletedTask;
+
+            if (msg.Content.Contains(' '))
+                cmdLength = msg.Content.IndexOf(' ');
+            else
+                cmdLength = msg.Content.Length;
+
+            cmd = msg.Content.Substring(1, cmdLength - 1);
+
             return Task.CompletedTask;
         }
     }
