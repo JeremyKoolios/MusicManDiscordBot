0582c0504e543d1818d6932f5dc373b8620c1575
JeremyKoolios <jeremy.koch@xtra.co.nz>
2021-11-27 01:12:54 +1300

    cleanup before making new branch

--- Modules/InfoModule.cs
+++ Modules/InfoModule.cs
@@ -1,13 +1,13 @@
 ﻿using Discord.Commands;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
 namespace DiscordMusicBot.Modules
 {
-    public class InfoModule
+    public class InfoModule : ModuleBase<SocketCommandContext>
     {
     }
 }
