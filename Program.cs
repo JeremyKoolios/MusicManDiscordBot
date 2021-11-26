dc9103e4b527a5868f2be197db164bf7e2f4c5af
JeremyKoolios <jeremy.koch@xtra.co.nz>
2021-11-27 00:42:34 +1300

    files added to repository, current working on CommandHandler.cs

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DiscordMusicBot
+{
+    public class Program
+    {
+        private DiscordSocketClient _client;
+
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+        public async Task MainAsync()
+        {
+            //initalize application
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            string _token = File.ReadAllText("C:/Users/jerem/Desktop/Dev/projects/DiscordMusicBot/token.txt");
+
+            await _client.LoginAsync(TokenType.Bot, _token);
+            await _client.StartAsync();
+
+            //keep application from closing
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
