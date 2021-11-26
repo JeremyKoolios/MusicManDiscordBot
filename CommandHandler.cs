0e4aab46827b9a6a007df8e08cfc6f42d2d38e8a
JeremyKoolios <jeremy.koch@xtra.co.nz>
2021-11-27 01:08:38 +1300

    added InfoModule

--- CommandHandler.cs
+++ CommandHandler.cs
@@ -1,10 +1,10 @@
 ﻿using Discord.Commands;
 using Discord.WebSocket;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
 using System.Reflection;
-using System.Text;
+//using System.Text;
 using System.Threading.Tasks;
 
 namespace DiscordMusicBot
