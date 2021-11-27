4dd4ddfc0f50782546ba4f44846ffe45306e10fb
JeremyKoolios <jeremy.koch@xtra.co.nz>
2021-11-27 18:01:40 +1300

    created Music module

---
+++ Modules/Music.cs
@@ -0,0 +1,20 @@
+﻿using Discord.Commands;
+using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordMusicBot.Modules
+{
+    public class Music : ModuleBase<SocketCommandContext>
+    {
+        [Command("join")]
+        [Summary("Joins a voice channel")]
+        public async Task Join()
+        {
+            Console.WriteLine("join command executed");
+            await Context.Channel.SendMessageAsync("pong!");
+        }
+    }
+}
