a0e3f31c391a8d46f51a639ecd5b845ae719e9ef
JeremyKoolios <jeremy.koch@xtra.co.nz>
2021-11-27 15:35:58 +1300

    basic command handler implemented

--- CommandHandler.cs
+++ CommandHandler.cs
@@ -1,13 +1,49 @@
-﻿//using System;
+﻿using Discord.Commands;
+using Discord.WebSocket;
+//using System;
 //using System.Collections.Generic;
 //using System.Linq;
+using System.Reflection;
 //using System.Text;
-//using System.Threading.Tasks;
+using System.Threading.Tasks;
 
 namespace DiscordMusicBot
 {
     public class CommandHandler
     {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
 
+        public CommandHandler(DiscordSocketClient client, CommandService commands)
+        {
+            _client = client;
+            _commands = commands;
+        }
+
+        public async Task InstallCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandsAsync;
+            await _commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(), services: null);
+        }
+
+        private async Task HandleCommandsAsync(SocketMessage messageParam)
+        {
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+
+            int argPos = 0;
+
+            if (!(message.HasCharPrefix('!', ref argPos) ||
+                message.HasMentionPrefix(_client.CurrentUser, ref argPos)) ||
+                message.Author.IsBot)
+                return;
+
+            var context = new SocketCommandContext(_client, message);
+
+            await _commands.ExecuteAsync(
+                context: context,
+                argPos: argPos,
+                services: null);
+        }
     }
 }
