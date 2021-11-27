417e4d4f36f49758ae2736fcb5bc069a203cf45d
JeremyKoolios <jeremy.koch@xtra.co.nz>
2021-11-27 18:21:11 +1300

    added a join command, bot can now join voice channel
    bug: bot leaves voice channel immediately after joining

--- Modules/Music.cs
+++ Modules/Music.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using System;
 //using System.Collections.Generic;
 //using System.Linq;
@@ -9,12 +10,23 @@ namespace DiscordMusicBot.Modules
 {
     public class Music : ModuleBase<SocketCommandContext>
     {
-        [Command("join")]
+        [Command("join", RunMode = RunMode.Async)]
         [Summary("Joins a voice channel")]
-        public async Task Join()
+        public async Task JoinChannel(IVoiceChannel voiceChannel = null)
         {
             Console.WriteLine("join command executed");
-            await Context.Channel.SendMessageAsync("pong!");
+            await Context.Channel.SendMessageAsync("*joins voice channel*");
+
+            // Get the audio channel that the user is in
+            voiceChannel = voiceChannel ?? (Context.User as IGuildUser)?.VoiceChannel;
+            if(voiceChannel == null)
+            {
+                await Context.Channel.SendMessageAsync("You must be in a voice channel to use this command");
+                return;
+            }
+
+            // Join voice channel
+            var audioClient = await voiceChannel.ConnectAsync();
         }
     }
 }
