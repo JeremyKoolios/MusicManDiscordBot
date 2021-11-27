8800933f68c7a08fe9c6ed9013c0fc0415bc036f
JeremyKoolios <jeremy.koch@xtra.co.nz>
2021-11-27 16:12:03 +1300

    added basic ping command

--- Modules/Test.cs
+++ Modules/Test.cs
@@ -1,14 +1,20 @@
 ﻿using Discord.Commands;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
 using System.Threading.Tasks;
 
 namespace DiscordMusicBot.Modules
 {
     public class Test : ModuleBase<SocketCommandContext>
     {
-
+        [Command("ping")]
+        [Summary("Responds with pong!")]
+        public async Task Pong()
+        {
+            Console.WriteLine("ping command executed");
+            await Context.Channel.SendMessageAsync("pong!");
+        }
     }
 }
