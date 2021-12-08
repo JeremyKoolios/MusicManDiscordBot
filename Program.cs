55da80316b5e4f93525f471422ab5950e8230805
JeremyKoolios <jeremy.koch@xtra.co.nz>
2021-12-09 00:26:21 +1300

    removed most of bot that relied on discord.NET framework

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿using Discord;
-using Discord.Commands;
-using Discord.WebSocket;
-using System;
+﻿using System;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -9,36 +6,13 @@ namespace DiscordMusicBot
 {
     public class Program
     {
-        private DiscordSocketClient _client;
-        private CommandService _commands;
-        private CommandHandler _commandHandler;
-
         static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
         public async Task MainAsync()
         {
-            // Initalize application
-            _client = new DiscordSocketClient();
-            _client.Log += Log;
-
             string token = File.ReadAllText("C:/Users/jerem/Desktop/Dev/projects/DiscordMusicBot/bin/Debug/net5.0/token.txt");
-
-            await _client.LoginAsync(TokenType.Bot, token);
-            await _client.StartAsync();
-
-            // CommandHandling
-            _commands = new CommandService(); // CommandService is a framework for commands. It automatically gets Discord messages
-
-            _commandHandler = new CommandHandler(_client, _commands);
-            await _commandHandler.InstallCommandsAsync();
 
             // Keep application from closing
             await Task.Delay(-1);
-        }
-
-        private Task Log(LogMessage msg)
-        {
-            Console.WriteLine(msg.ToString());
-            return Task.CompletedTask;
         }
     }
 }
