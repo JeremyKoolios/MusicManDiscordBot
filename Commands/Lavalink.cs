fb51054b996e39951fd015e37fc8ab1b7ee09dca
JeremyKoolios <jeremy.koch@xtra.co.nz>
2021-12-10 00:22:35 +1300

    added join command

--- Commands/Lavalink.cs
+++ Commands/Lavalink.cs
@@ -1,8 +1,11 @@
-﻿using DSharpPlus.CommandsNext;
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+using DSharpPlus.Lavalink;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,13 +13,28 @@ namespace DiscordMusicBot.Commands
 {
     public class Lavalink : BaseCommandModule
     {
-        [Command("join")]
+        [Command]
         public async Task Join(CommandContext ctx, DiscordChannel channel)
         {
+            var lava = ctx.Client.GetLavalink();
+            if (!lava.ConnectedNodes.Any())
+            {
+                await ctx.RespondAsync("The Lavalink connection is not established");
+                return;
+            }
 
+            var node = lava.ConnectedNodes.Values.First();
+            if(channel.Type != ChannelType.Voice)
+            {
+                await ctx.RespondAsync("You need to connect to a voice channel");
+                return;
+            }
+
+            await node.ConnectAsync(channel);
+            await ctx.RespondAsync($"Joined {channel.Name}");
         }
 
-        [Command("leave")]
+        [Command]
         public async Task Leave(CommandContext ctx, DiscordChannel channel)
         {
 
