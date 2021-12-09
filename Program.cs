8e25b194c9bb67774f7599d4bbf9390ba4dda6eb
JeremyKoolios <jeremy.koch@xtra.co.nz>
2021-12-09 18:35:46 +1300

    added command which uses DiscordMessageBuilder

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -20,15 +23,38 @@ namespace DiscordMusicBot
             {
                 Token = File.ReadAllText("token.txt"),
                 TokenType = TokenType.Bot,
-                Intents = DiscordIntents.AllUnprivileged
+                Intents = DiscordIntents.All, // Limits the type of events that the bot receives
+                MinimumLogLevel = LogLevel.Debug
             });
             await discordClient.ConnectAsync();
 
-            // ping command
+            // Message created event handled by lambda expression
+            // s contains instance of object that fired the event,
+            // e contains arguments for specific event that you're handling
             discordClient.MessageCreated += async (s, e) =>
             {
+                // ping command
                 if (e.Message.Content.ToLower().StartsWith("ping"))
                     await e.Message.RespondAsync("pong!");
+
+                // daniel is epic command
+                // Use _ = Task.Run(async () => { code here } when dealing with commands that take long to run to prevent deadlocks)
+                _ = Task.Run(async () =>
+                {
+                    if (e.Message.Content.ToLower().StartsWith("daniel is epic"))
+                    {
+                        using (FileStream fs = new FileStream("C:/Users/jerem/Pictures/hotdoggy.png", FileMode.Open, FileAccess.Read))
+                        {
+                            DiscordMessage msg = await new DiscordMessageBuilder()
+                            .WithContent($"That's right is indeed epic {e.Author.Mention}")
+                            .WithFiles(new Dictionary<string, Stream>() { { "C:/Users/jerem/Pictures/hotdoggy.png", fs } })
+                            .WithReply(e.Message.Id)
+                            .WithAllowedMentions(new IMention[] { new UserMention(e.Author) })
+                            .SendAsync(e.Channel);
+                        }
+                    }
+                });
+                
             };
 
 
