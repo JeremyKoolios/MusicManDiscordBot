458731458b1ec047513d670adc83fe7d05653a7e
JeremyKoolios <jeremy.koch@xtra.co.nz>
2021-12-09 19:34:17 +1300

    basic CommandContext implemented

---
+++ Commands/MyFirstModule.cs
@@ -0,0 +1,19 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DiscordMusicBot.Commands
+{
+    public class MyFirstModule : BaseCommandModule
+    {
+        [Command("greet")]
+        public async Task GreetCommand(CommandContext ctx)
+        {
+            await ctx.RespondAsync("Howdy stranger");
+        }
+    }
+}
