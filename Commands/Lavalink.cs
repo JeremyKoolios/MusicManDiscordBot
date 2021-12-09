6e50f77b0e8363797e9cce6fbd9d4bcfaf7f36d6
JeremyKoolios <jeremy.koch@xtra.co.nz>
2021-12-10 00:12:07 +1300

    added lavalink connection and module

---
+++ Commands/Lavalink.cs
@@ -0,0 +1,25 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordMusicBot.Commands
+{
+    public class Lavalink : BaseCommandModule
+    {
+        [Command("join")]
+        public async Task Join(CommandContext ctx, DiscordChannel channel)
+        {
+
+        }
+
+        [Command("leave")]
+        public async Task Leave(CommandContext ctx, DiscordChannel channel)
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿using DiscordMusicBot.Commands;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
-using DSharpPlus.Entities;
+using DSharpPlus.Lavalink;
+using DSharpPlus.Net;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -15,11 +15,11 @@ namespace DiscordMusicBot
     {
         static void Main(string[] args)
         {
-            MainAsync().GetAwaiter().GetResult();
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
         }
 
         // Actual Main function
-        static async Task MainAsync()
+        static async Task MainAsync(string[] args)
         {
             // Application configuration
             var discord = new DiscordClient(new DiscordConfiguration()
@@ -29,26 +29,45 @@ namespace DiscordMusicBot
                 Intents = DiscordIntents.All, // Limits the type of events that the bot receives
                 MinimumLogLevel = LogLevel.Debug
             });
+
+            // Lavalink
+            var lavalinkEndpoint = new ConnectionEndpoint
+            {
+                // From Lavalink\application.yml
+                Hostname = "127.0.0.1",
+                Port = 2333
+            };
+            var lavalinkConfig = new LavalinkConfiguration
+            {
+                // Password from Lavalink\application.yml
+                Password = "youshallnotpass",
+                RestEndpoint = lavalinkEndpoint,
+                SocketEndpoint = lavalinkEndpoint
+            };
+            var lavalink = discord.UseLavalink();
+
+            // Services
             var services = new ServiceCollection()
                 .AddSingleton<Random>()
                 .BuildServiceProvider();
+
+            // Commands
             var commands = discord.UseCommandsNext(new CommandsNextConfiguration()
             {
                 StringPrefixes = new string[] { "!" },
                 Services = services
             });
-
-            // Register modules
             commands.RegisterCommands<MyFirstModule>();
+            commands.RegisterCommands<Lavalink>();
 
-            // Connect client to discord
+            // Connect to discord then lavalink
             await discord.ConnectAsync();
+            await lavalink.ConnectAsync(lavalinkConfig);
 
             
 
 
 
-            // Keep application from closing
             await Task.Delay(-1);
         }
 
